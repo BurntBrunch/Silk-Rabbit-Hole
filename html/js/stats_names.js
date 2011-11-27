2342bb60e85326dcadf51ca6491aa6840bb7b6ec
Delyan Kratunov <delyan.kratunov@gmail.com>
2011-11-27 17:14:34 +0000

    Initial map version

---
+++ html/js/stats_names.js
@@ -0,0 +1,8 @@
+stats_country_names = {
+               'num_listings': "# of listings",
+               'num_users': "# of sellers",
+               'percent_all_listings': "% of all listings with country",
+               'drugs_distribution': "Drug type breakdown",
+               'top_five_users': "Top 5 sellers"}
+
+stats_country_order = ["num_users", "num_listings", "percent_all_listings",  "top_five_users", "drugs_distribution"]
